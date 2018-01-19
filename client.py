7522f29e6ea068d18238bf55fd6ea979a8afedcf
WangHexie <yuge0099@gmail.com>
2018-01-19 11:07:59 +0800

    try to handle more situation like ipv6

--- client.py
+++ client.py
@@ -5,13 +5,13 @@ import select
 # thread
 
 localsock = 1081
-
+listenNumber = 1500
 
 
 def tcpServer():
     socketserver = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     socketserver.bind(('0.0.0.0', localsock))
-    socketserver.listen(1500)
+    socketserver.listen(listenNumber)
     while True:
         sock, addr = socketserver.accept()
         print(str(addr)+'connect')
@@ -27,86 +27,72 @@ def tcpServer():
 
 #sockethandler
 def sockHandler(sock, addr):
-    data = sock.recv(4096)
+    data = sock.recv(4096)                    #this hasn't been used for the reason down there
     print(str(addr))
-    print(data)
-    sock.send(b'\x05\x00')                 #what hapened here and up there??????
+    sock.send(b'\x05\x00')                 #other situation is out of my ability ,since i have no problem in using it .
     addressRecv = sock.recv(4096)
-    print(addressRecv)
-#get address
-    address =socket.gethostbyname(addressRecv[5:-2])
+#get  adrress
+    try:
+        addrtype = addressRecv[3]
+        if addrtype == 1:
+            addr = socket.inet_ntoa(addressRecv[5:-2])
+        elif addrtype == 3:
+            addr = socket.gethostbyname(addressRecv[5:-2])
+        elif addrtype == 4:                                              #socke type have to change to support ipv6
+            addr = socket.inet_ntop(socket.AF_INET6, addressRecv[5:-2])
+        else:
+            # not support
+            print('addr_type not support')
+            return
+        port = addressRecv[-2] * 256 + addressRecv[-1]
 
-    port = addressRecv[-2] * 256 + addressRecv[-1]
+        situ, sockweb = tcpTest(addr, port, sock)
+        sock.send(b'\x05' + situ)
+        handle_tcp(sock, sockweb)
+    except socket.error :
+        print('socket errot')
 
-    print(str(address)+':'+str(port))
 
-    situ ,sockweb = tcpTest(address,port,sock)
-    sock.send(b'\x05'+situ)
+#copy from shadowsocks . Have to change it later!!!!!!
+def send_all(sock, data):
+    bytes_sent = 0
+    while True:
+        r = sock.send(data[bytes_sent:])
+        if r < 0:
+            return r
+        bytes_sent += r
+        if bytes_sent == len(data):
+            return bytes_sent
 
-    handle_tcp(sock, sockweb)
-    '''  
-    data =b''
-    z = sock.recv(4096)
-    print('zzzzzzzzzzzzzzzzzzzzz')
-    print(z)
-      
-    while z != '':
-        data=data +z
-        z=sock.recv(4096)
-    print('data!!!!!!!!!!!!')
-    print(data)
-    '''
-
-    '''
-    while data != '':
-        if sockweb.send(data) <=0:
-            break
-
-        data2 = b''
-        z2 = sockweb.recv(4096)
-        while z2 != '':
-            data2 = data2 + z2
-            z2 = sockweb.recv(4096)
-        print('data2')
-        print(data2)
-        if sock.send(data2) <= 0:
-            break
-
-        data = b''
-        z = sock.recv(4096)
-
-        while z != '':
-            data = data + z
-            z = sock.recv(4096)
-        print('data!!!!!!!!!!!!!!!!')
-        print(data)
-    '''
-
-#copy from shadowsocks
 def handle_tcp(sock, remote):
-        try:
-            fdset = [sock, remote]
-            while True:
-                r, w, e = select.select(fdset, [], [])
-                if sock in r:
-                    if remote.send(sock.recv(4096)) <= 0:
-                        break
-                if remote in r:
-                    if sock.send(remote.recv(4096)) <= 0:
-                        break
-        finally:
-            remote.close()
+    try:
+        fdset = [sock, remote]
+        while True:
+            r, w, e = select.select(fdset, [], [])
+            if sock in r:
+                data = sock.recv(4096)
+                if len(data) <= 0:
+                    break
+                result = send_all(remote, data)
+                if result < len(data):
+                    raise Exception('failed to send all data')
+            if remote in r:
+                data = remote.recv(4096)
+                if len(data) <= 0:
+                    break
+                result = send_all(sock, data)
+                if result < len(data):
+                    raise Exception('failed to send all data')
+    finally:
+        sock.close()
+        remote.close()
 
-
-#tcp connet for test use  /have to change
+#tcp connet for test use  /have to change   get
 def tcpTest(address,port,sock):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock_name = sock.getsockname()
     server_hex_addr = socket.inet_aton(sock_name[0])
-
-
     server_hex_port = port_to_hex_string(sock_name[1])
-
     print(server_hex_port)
     try:
         s.connect((address, port))
@@ -117,15 +103,7 @@ def tcpTest(address,port,sock):
         return b'\x04\x00\x01'+server_hex_addr+server_hex_port ,s
     return b'\x00\x00\x01'+server_hex_addr+server_hex_port ,s
 
-def int_to_bytes(value, length):
-    result = []
 
-    for i in range(0, length):
-        result.append(value >> (i * 8) & 0xff)
-
-    result.reverse()
-
-    return result
 #port_to_hex_string done by copy
 def port_to_hex_string(int_port):
     port_hex_string = bytes([int_port//256])+bytes([int_port%256])
